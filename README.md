16bb437d693163075172860f50ceab503df6835f
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-08-13 16:48:41 +0300

    added remove and lookup functions and tests to hash table

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 ## Collection of Data Structures and Algorithms in Javascript
 
 * Reverse String
-  - With Methods
-  - With Loop
-  - Recursively
 
 * Queues
 
 * Binary Search Tree
+
+* Hash Table
--- hash-table.js
+++ hash-table.js
@@ -11,7 +11,7 @@ let hash = (string, max) => {
 let hashTable = function() {
   
   let storage = [];
-  const storageLimit = 4;
+  const storageLimit = 15;
 
   // Helper function
   this.print = function() {
@@ -42,4 +42,43 @@ let hashTable = function() {
       }
     }
   };
-}
+
+  // Remove key from hash table
+  this.remove = function(key) {
+    let index = hash(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  }
+
+  // Look up the item
+  this.lookup = function(key) {
+    let index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+};
+
+// console.log(hash('John', 10000))
+
+let ht = new hashTable();
+ht.add('Tom', 'person');
+ht.add('Sven', 'dog');
+ht.add('McLaren', 'car');
+ht.add('Pizza', 'dish')
+console.log(ht.lookup('Sven'))
+ht.print();
+
