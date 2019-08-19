17f21e69bcb08a9ead7a003648056d04dabd1ecb
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-08-19 23:29:14 +0300

    Updates

--- queues.js
+++ queues.js
@@ -1,48 +1,48 @@
-// function Queue () {
-//   let collection = [];
-//   // Helper Function
-//   this.print = function() {
-//     console.log(collection);
-//   }
-//   this.enqueue = function(element) {
-//     collection.push(element);
-//   }
-//   this.dequeue = function() {
-//     collection.shift();
-//   }
-//   this.front = function() {
-//     return collection[0];
-//   }
-//   this.size = function() {
-//     return collection.length;
-//   }
-//   this.isEmpty = function() {
-//     return (collection.length === 0);
-//   }
-// }
+function Queue () {
+  let collection = [];
+  // Helper Function
+  this.print = function() {
+    console.log(collection);
+  }
+  this.enqueue = function(element) {
+    collection.push(element);
+  }
+  this.dequeue = function() {
+    collection.shift();
+  }
+  this.front = function() {
+    return collection[0];
+  }
+  this.size = function() {
+    return collection.length;
+  }
+  this.isEmpty = function() {
+    return (collection.length === 0);
+  }
+}
 
-// let q = new Queue();
+let q = new Queue();
 
-// // Queue items
-// q.enqueue('Tom');
-// q.enqueue('James');
-// q.enqueue('Tiffany');
-// q.enqueue('Rosalinda');
+// Queue items
+q.enqueue('Tom');
+q.enqueue('James');
+q.enqueue('Tiffany');
+q.enqueue('Rosalinda');
 
-// // Print Queue to check it
-// q.print();
+// Print Queue to check it
+q.print();
 
-// // Dequeue first item
-// q.dequeue();
+// Dequeue first item
+q.dequeue();
 
-// // Print Queue to check it
-// q.print();
+// Print Queue to check it
+q.print();
 
-// // Check item in front of queue, size of queue, if queue is empty
-// // Queue = array
-// console.log(q.front());
-// console.log(q.size());
-// console.log(q.isEmpty());
+// Check item in front of queue, size of queue, if queue is empty
+// Queue = array
+console.log(q.front());
+console.log(q.size());
+console.log(q.isEmpty());
 
 
 
--- reverse-string.js
+++ reverse-string.js
@@ -6,18 +6,18 @@
 
 // Reverse string with built in functions
 
-// function reverseString(str) {
-//   let originalStr = str.split("")
-//   console.log(originalStr)
+function reverseString(str) {
+  let originalStr = str.split("")
+  console.log(originalStr)
 
-//   let reverseStr = originalStr.reverse();
-//   console.log(reverseStr)
+  let reverseStr = originalStr.reverse();
+  console.log(reverseStr)
 
-//   let joinStr = reverseStr.join("");
-//   console.log(joinStr)
+  let joinStr = reverseStr.join("");
+  console.log(joinStr)
 
-// }
-// reverseString('Hello')
+}
+reverseString('Hello')
 
 
 // _________________________________________________________________________
@@ -25,27 +25,27 @@
 
 // Reverse string with loop
 
-// let letters = [];
+let letters = [];
 
-// let word = "amazing"
+let word = "amazing"
 
-// let rword = "";
+let rword = "";
 
-// // Push each letter into array
-// for (let i = 0; i < word.length; i++) {
-//   letters.push(word[i]);
-// }
+// Push each letter into array
+for (let i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
 
-// // pop off the array in reverse order
-// for (let i = 0; i < word.length; i++) {
-//   rword += letters.pop();
-// }
+// pop off the array in reverse order
+for (let i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
 
-// if(rword === word) {
-//   console.log(`${word} and ${rword} is a palindrome`);
-// } else {
-//   console.log(`${word} and ${rword} is not a palindrome`);
-// }
+if(rword === word) {
+  console.log(`${word} and ${rword} is a palindrome`);
+} else {
+  console.log(`${word} and ${rword} is not a palindrome`);
+}
 
 
 // _________________________________________________________________________
@@ -53,12 +53,12 @@
 
 // Reverse string recursively
 
-// function reverseString(str) {
-//   if (str === "") {
-//     return "";
-//   } else {
-//     return reverseString(str.substr(1)) + str.charAt(0); 
-//   } 
-// }
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0); 
+  } 
+}
 
-// reverseString('Hello')
+reverseString('Hello')
