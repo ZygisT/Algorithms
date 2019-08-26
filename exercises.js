dd2c5e217fd494ce67be39855d361a5162cf0cae
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-08-26 22:03:11 +0300

    Added explanation to closure function

--- exercises.js
+++ exercises.js
@@ -13,13 +13,22 @@ test(myArr, () => console.log('Array has been modified'));
 
 // Js Closures
 
-// Get the secret
+// Get the private variable (secret code)
+
+/* 
+Explanation: You cannot access variable in the parent (hiddenCode) function from outside.
+You need to create a helper function (child function) which is called nested function
+which creates a closure. Child function (closure) has access to local (child), outer (parent) and global scopes.
+*/
 
 function hiddenCode() {
+
   let code = "You\'ve found a secret code"
+
   return function totallyNotSecretCode() {
     return code
   }
+  
 }
 
 const getCode = hiddenCode()
