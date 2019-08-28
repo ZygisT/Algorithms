62cce8e51b871c76a148711a07d7f5fbf0913179
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-08-28 18:14:10 +0300

    Added letter replace

--- exercises.js
+++ exercises.js
@@ -57,3 +57,28 @@ function findWord(word) {
 
 console.log(findWord(' John and Mary bought 5 apples and 3 oranges '))
 
+
+// Replace letters and capitalize vowels
+
+function replaceLetters(word) {
+  
+  // replace for each letter a-z exacute a callback function
+  // which cheks if letter is z if true return a
+  // if false return a string from charCode method + 1
+  // charCode is a number that a letter represents
+  // +1 to charCode number means moving to the next letter
+  let converted = word.replace(/[a-z]/gi, (char) => {
+    return char === 'z' || char === 'Z' ? 'a' : String.fromCharCode(char.charCodeAt() + 1)
+  })
+  
+  // Execute callback function on all provided vowels
+  // Use toUpperCase method on each letter
+  let capitalized = converted.replace(/a|e|i|o|u/gi, (vowel) => {
+    return vowel.toUpperCase();
+  })
+ 
+  return capitalized
+}
+
+console.log(replaceLetters('Pizza'));
+
